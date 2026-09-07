cbaa045c4364296f8f2842f188eb7e03f25e74ff
Rahulsirvi0 <rahulsirvi414@gmail.com>
2026-09-08 00:32:24 +0530

    Photos chnages

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -2,8 +2,8 @@
 export const teamMembers = [
   { id: 1, name: 'Sristhi Ray', position: 'Chairperson', avatar: '/Srishthi.jpeg' , Instagram: 'https://www.instagram.com/__sri.t/', Linkedin: 'https://www.linkedin.com/in/sristhi-m-ray-18894a337/'},
   { id: 2, name: 'Aniket Yadav', position: 'Vice Chair', avatar: '/Aniket.jpeg' , Linkedin: 'https://www.linkedin.com/in/aniket-yadav-036714338/'},
-  { id: 3, name: 'Raj Gupta', position: 'Secretary', avatar: '/Raj.jpeg' , Instagram: 'https://www.instagram.com/_raj._gupta/', Linkedin: 'https://www.linkedin.com/in/rajgupta2701/'},
+  { id: 3, name: 'Raj Gupta', position: 'Secretary', avatar: '/Raj.JPG' , Instagram: 'https://www.instagram.com/_raj._gupta/', Linkedin: 'https://www.linkedin.com/in/rajgupta2701/'},
   { id: 4, name: 'Maitrey Suthar', position: 'Treasurer', avatar: '/Maitrey.jpeg', Instagram: 'https://www.instagram.com/_maitrey_s/', Linkedin: 'https://www.linkedin.com/in/maitrey-suthar-210639315/'},
-  { id: 5, name: 'Rahul Choudhary', position: 'Webmaster', avatar: '/Rahul C.jpeg' , Instagram: 'https://www.instagram.com/rahul_sirvi0/', Linkedin: 'https://www.linkedin.com/in/rahul-choudhary-3905b5316/'},
-  { id: 6, name: 'Rahul Singh', position: 'Event Coordinator', avatar: '/Rahul S.jpeg' , Instagram: 'https://www.instagram.com/r.k.vijaysingh22/', Linkedin: 'https://www.linkedin.com/in/rahul-vijaykumar-singh-85895532b/'},
+  { id: 5, name: 'Rahul Choudhary', position: 'Webmaster', avatar: '/Rahul C.jpg' , Instagram: 'https://www.instagram.com/rahul_sirvi0/', Linkedin: 'https://www.linkedin.com/in/rahul-choudhary-3905b5316/'},
+  { id: 6, name: 'Rahul Singh', position: 'Event Coordinator', avatar: '/Rahul S.JPG' , Instagram: 'https://www.instagram.com/r.k.vijaysingh22/', Linkedin: 'https://www.linkedin.com/in/rahul-vijaykumar-singh-85895532b/'},
 ];
